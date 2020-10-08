f258e205a5bd7cf022a71561426095d64be253df
rizwan <rizwan.tasveen@gmail.com>
2020-10-08 23:22:48 +0530

    space

--- app/src/main/java/com/necto/fruitgroceryapp/ui/main/view/activity/SplashActivity.kt
+++ app/src/main/java/com/necto/fruitgroceryapp/ui/main/view/activity/SplashActivity.kt
@@ -13,10 +13,8 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
